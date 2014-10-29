98d99ca76ae82c6a3cbab4cf777c15d176053478
Lukáš Dočkal <Lokishek@seznam.cz>
2014-10-29 20:56:55 +0100

    práce se zadáními

--- init.py
+++ init.py
@@ -1,14 +1,10 @@
+# -*- coding: utf8 -*-
 from flask import Flask , redirect , render_template , request, session, flash
 from functools import wraps
+from os import walk , urandom , listdir
 
 app = Flask(__name__)
-app.secret_key = "sad2dfhvcbsd3258"
-
-
-#redirect visitors to index and load Login 
-@app.route("/")
-def get_welcome():
-    return redirect("index")
+app.secret_key = urandom(24)
 
 def Login_required(test):
     @wraps(test)
@@ -20,7 +16,12 @@ def Login_required(test):
             return redirect("/index")
     return wrap
 
-@app.route("/index" , methods=["post","get"])
+
+@app.route("/")
+def get_welcome():
+    return redirect("index")
+
+@app.route("/index" , methods=["POST","GET"])
 def try_login_pass():
     if request.method == "POST":
         if request.form["name"] != "admin" or request.form["password"] != "aaaa":
@@ -39,10 +40,40 @@ def logOut():
             
             
 @app.route("/Main_page")
-@Login_required
-def main_page():
-    return render_template("Main_page.html")
-    
+#@Login_required
+def main_page_for_admin():
+    return render_template("Basic_template.html")
+
+@app.route("/Entering", methods=["POST","GET"])
+#@Login_required
+def list_of_works():
+    list_of_works=[]
+    for (root, dirs, files) in walk('works'):
+        list_of_works=list_of_works+files                
+    return render_template("Entering.html",list_of_works=list_of_works)
+
+@app.route("/Entering/<name_of_work>", methods=["POST","GET"])
+def work_with_object(name_of_work):
+    way_to_file = "works/%s" %name_of_work
+    if request.method == "POST":
+        
+        #Zobrazí obsah souboru Name_of_work v defaultní složce "works"
+        if request.form.get("display",None) == "Zobrazit":
+                view_work = open(way_to_file, 'r')
+                return view_work.read()
+            
+        #Povolí načítání do klasických uživatelských účtů    
+        elif request.form.get("post_on",None) == "Zveřejnit":
+            return "zveřejni"
+        
+        #Čtení/zápis povolen,vypíše do textboxu a násladně přepíše soubor
+        elif request.form.get("edit",None) == "Upravit":
+            return "uprav"
+        
+        #Vymaže soubor,musí vyskočit potvrzovací tabulka
+        elif request.form.get("delet",None) == "Smazat":
+            return "smaž"
+ 
             
 
 app.run(debug=True)

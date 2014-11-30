9d1bc9733e97af48a4283aec76129485d5527c57
Lokky <lokishek@seznam.cz>
2014-11-30 19:05:27 +0100

    Vytvořeno uživatelské prostředi

--- init.py
+++ init.py
@@ -9,16 +9,29 @@ from os import walk, urandom
 
 import markdown
 import codecs
-import sqlite3
+import os
+from datetime import datetime
 
-md_ext = ['markdown.extensions.extra',
-          'work_syntax:WorkSyntax'
-          ]
+md_ext1 = ['markdown.extensions.extra',
+           'work_syntax_admin:WorkSyntax']
+
+md_ext2 = ['markdown.extensions.extra',
+           'work_syntax_user:WorkSyntax']
 
 app = Flask(__name__)
 app.secret_key = urandom(24)
 
-def Login_required(test):
+def Login_required_for_admin(test):
+    @wraps(test)
+    def wrap(*arg, **kwargs):
+        if "logged_in" in session and "access" in session:
+            return test(*arg, **kwargs)
+        else:
+            flash("Nedostatečná práva...")
+            return redirect("/index")
+    return wrap
+
+def Login_required_for_user(test):
     @wraps(test)
     def wrap(*arg, **kwargs):
         if "logged_in" in session:
@@ -38,23 +51,19 @@ def try_login_pass():
         password = request.form["password"]
         Login = request.form["name"]
 
-        conn = sqlite3.connect('static/Database_of_users.db')
-        c = conn.cursor()
+        if Login == "user" and password == "user":
+            print ("prihlasen Uživatel")
+            session["user"] = Login
+            session["logged_in"] = True
+            return redirect("Main_user")
 
-        c.execute("""select * FROM Users WHERE passw =:pass
-                     and Login=:Log""", {"Log": Login, "pass": password})
-        Data = c.fetchone()
-        c.close()
+        elif Login == "admin" and password == "aaaa":
+            print ("prihlášen Admin")
+            session["user"] = Login
+            session["access"] = True
+            session["logged_in"] = True
+            return redirect("Main_admin")
 
-        if Data:
-            if Login != "Admin":
-                session["user"] = Login
-                session["logged_in"] = True
-                return redirect("Main_user")
-            else:
-                session["user"] = Login
-                session["logged_in"] = True
-                return redirect("Main_admin")
         else:
             flash("Neplatny Login nebo Heslo")
     return render_template("index.html")
@@ -62,16 +71,79 @@ def try_login_pass():
 @app.route("/LogOut")
 def logOut():
     session.pop("logged_in", None)
+    session.pop("access", None)
     flash("Byl jste odhlasen")
     return redirect("index")
 
 @app.route("/Main_admin")
-@Login_required
+@Login_required_for_admin
 def main_page_for_admin():
     return render_template("Basic_template.html")
 
+@app.route("/Main_user")
+@Login_required_for_user
+def Main_page_for_user():
+    list_works = []
+    list_dir = []
+
+    for (root, dirs, files) in walk('works'):
+        for dir in dirs:
+            for file in os.listdir("works/%s" % dir):
+                if file.endswith(".txt"):
+                    dec = "works/%s/%s" % (dir, file)
+                    fil = open(dec, "r")
+                    f = fil.readline()
+                    tag, datum_od, cas_od, datum_do, cas_do = f.split()
+
+                    den_start, mesic_start, rok_start = datum_od.split(".")
+                    hod_start, min_start = cas_od.split(":")
+
+                    den_konec, mesic_konec, rok_konec = datum_do.split(".")
+                    hod_konec, min_konec = cas_do.split(":")
+
+
+                    date_od = datetime(int(rok_start), int(mesic_start),
+                                       int(den_start), int(hod_start),
+                                       int(min_start))
+
+                    date_do = datetime(int(rok_konec), int(mesic_konec),
+                                       int(den_konec), int(hod_konec),
+                                       int(min_konec))
+
+                    date_start = date_od.strftime("%Y-%m-%d %H:%M")
+                    date_end = date_do.strftime("%Y-%m-%d %H:%M")
+                    now = datetime.now()
+                    print (file)
+                    print ("start", date_start)
+                    print ("-DNES", now)
+                    print ("konec", date_end)
+                    if (date_od < now < date_do) is True:
+                        list_dir.append(dec)
+                        list_works.append(file)
+
+                    fil.close()
+    print (list_works)
+    print (list_dir)
+    return render_template("basic_user_list.html", list_works=list_works,
+                           list_dir=list_dir)
+
+@app.route("/Main_user/<base>/<name_user_dir>/<name_user_file>")
+@Login_required_for_user
+def Work_user(base, name_user_dir, name_user_file):
+    print (name_user_dir)
+    print (name_user_file)
+    if request.method == "POST":
+        print(request.form["number"])
+        print(request.form["choice"])
+        print(request.form["text"])
+
+    view_work = codecs.open("%s/%s/%s" % (base, name_user_dir, name_user_file),
+                            'r', 'utf-8')
+    view_work = markdown.markdown(view_work.read(), md_ext2)
+    return render_template('work_user.html', work=view_work)
+
 @app.route("/Entering", methods=["POST", "GET"])
-@Login_required
+@Login_required_for_admin
 def list_of_works():
     list_of_dirs = []
 
@@ -82,7 +154,7 @@ def list_of_works():
 
 
 @app.route("/Entering/<path:name_of_work>", methods=["POST", "GET"])
-@Login_required
+@Login_required_for_admin
 def open_class(name_of_work):
     list_of_files = []
 
@@ -92,8 +164,9 @@ def open_class(name_of_work):
     return render_template("Entering.html", name_of_work=name_of_work,
                            list_of_files=list_of_files)
 
-@app.route("/Entering/<name_of_work>/<path:name_of_files>", methods=["POST", "GET"])
-#@Login_required
+@app.route("/Entering/<name_of_work>/<path:name_of_files>",
+           methods=["POST", "GET"])
+@Login_required_for_admin
 def open_file(name_of_work, name_of_files):
     if request.method == "POST":
         print(request.form["number"])
@@ -102,12 +175,7 @@ def open_file(name_of_work, name_of_files):
 
     view_work = codecs.open("works/%s/%s" % (name_of_work, name_of_files), 'r',
                             'utf-8')
-    view_work = markdown.markdown(view_work.read(), md_ext)
+    view_work = markdown.markdown(view_work.read(), md_ext1)
     return render_template('work.html', work=view_work)
-
-@app.route("/Main_user")
-#@Login_required
-def Main_page_for_user():
-    return render_template("Basic_user.html")
 
 app.run(debug=True)
---
+++ work_syntax_admin.py
@@ -0,0 +1,67 @@
+# -*- coding: utf8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import generators
+
+from markdown.preprocessors import Preprocessor
+from markdown.extensions import Extension
+import re
+
+
+class WorkSyntaxPreprocessor(Preprocessor):
+    def run(self, lines):
+
+        new_lines = ['''<form action="" method="post">''']
+
+        task = re.compile('^::task (.+)$')
+        date = re.compile('^::date (.+)$')
+        op = re.compile('^::open')
+        num = re.compile('^::number (.+)$')
+        lb_f = re.compile('^:- (.+)$')
+        lb_t = re.compile('^:\+ (.+)$')
+
+        for line in lines:
+
+            match_task = task.search(line)
+            match_date = date.search(line)
+            match_op = op.search(line)
+            match_number = num.search(line)
+            match_false = lb_f.search(line)
+            match_true = lb_t.search(line)
+
+            if match_task:
+                new_lines.append('<h2>{0}</h2></optgroup>'
+                                 .format(match_task.group(1)))
+            elif match_date:
+                new_lines.append('<h5>{0}</h5>'
+                                 .format(match_date.group(1)))
+            elif match_op:
+                new_lines.append('''<br><textarea name="text" cols="40"
+                                rows="4"></textarea>''')
+
+            elif match_number:
+                new_lines.append('''<br><input type="text"
+                                name="number" value="">
+                                <span style="color: red">
+                                {0}</span>'''
+                                 .format(match_number.group(1)))
+
+            elif match_false:
+                new_lines.append('''<br><input type="radio" name="choice"
+                                 value="{0}"> {0}'''
+                                 .format(match_false.group(1)))
+
+            elif match_true:
+                new_lines.append('''<br><span style="color: red"><input type="radio" name="choice"
+                                 value="{0}"> {0}</span>'''
+                                 .format(match_true.group(1)))
+            else:
+                new_lines.append(line)
+        new_lines.append('''<br><input type="submit"
+                        value ="Odeslat"></form>''')
+        return new_lines
+
+class WorkSyntax(Extension):
+    def extendMarkdown(self, md, md_globals):
+        md.preprocessors.add('urlify', WorkSyntaxPreprocessor(md), '_end')
